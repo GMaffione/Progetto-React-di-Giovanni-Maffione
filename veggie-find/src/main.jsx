c984d8a47a7f37a9c727ca050f77157f8e184f85
GMaffione <maffionegiovanni0@gmail.com>
2026-07-24 11:25:08 +0200

    Add files via upload

--- veggie-find/src/main.jsx
+++ veggie-find/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import * as Sentry from "@sentry/react";
 import { RecipeProvider } from "./context/RecipeContext";
 import App from "./App";
 import "./index.css";
+
+// Inizializzazione Sentry per l'error tracking.
+// Il DSN viene letto da una variabile d'ambiente, mai scritto qui in chiaro.
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  sendDefaultPii: false,
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
